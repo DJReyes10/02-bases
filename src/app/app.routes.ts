ba23540b54c88a6177ba204a571ac979361d4a79
DJReyes10 <djreyesp@unah.hn>
2025-03-21 16:36:17 -0600

    Proyecto Angular (Counter)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { counterPagesComponent } from './pages/counter/counter-page.componet';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{
+    path: '',
+    component: counterPagesComponent
+}
+    
+];
