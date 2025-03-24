dff9e019e1956c4c952143d8e1b8a22bc5734379
DJReyes10 <djreyesp@unah.hn>
2025-03-24 16:37:55 -0600

    Se trabajo hasta la seccion de la tarea: Preparación de Paginas. Sección 5

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { counterPagesComponent } from './pages/counter/counter-page.component';
 import { HeroPageComponent } from './pages/hero/hero-page.component';
+import { DragonballPageComponent } from './pages/dragonball/dragonball-page.component';
 
 import { Component } from '@angular/core';
 
@@ -12,6 +13,13 @@ export const routes: Routes = [
   {
     path: 'hero',
     component: HeroPageComponent
-  }
-
+  },
+  {
+    path: 'dragon',
+    component: DragonballPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+},
 ];
---
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,17 @@
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
+@Component({
+  templateUrl: './dragonball-page.component.html',
+  imports: [
+    // NgClass
+  ],
+})
+export class DragonballPageComponent {
+}
