7db57f9ae702dd02eb0bde821ced9ce462849dc1
Douglas Reyes <djreyesp@unah.hn>
2025-03-24 22:20:14 -0600

    Clase @for avance

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -14,4 +14,10 @@ interface Character {
   ],
 })
 export class DragonballPageComponent {
+  characters = signal([
+    {id: 1, name:"Goku", power:9000},
+    {id: 1, name:"Vegeta", power:8000},
+    {id: 1, name:"Piccolo", power:5000}
+  ]);
+
 }
