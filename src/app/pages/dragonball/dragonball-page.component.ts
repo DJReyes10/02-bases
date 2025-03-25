57ad6fdff2dbcfb395dc878d9898e8015660a851
Douglas Reyes <djreyesp@unah.hn>
2025-03-24 23:13:48 -0600

    Utilización del @if y ngClass

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -17,7 +17,16 @@ export class DragonballPageComponent {
   characters = signal([
     {id: 1, name:"Goku", power:9000},
     {id: 1, name:"Vegeta", power:8000},
-    {id: 1, name:"Piccolo", power:5000}
+    {id: 1, name:"Piccolo", power:5000},
+    {id: 1, name:"Gohan", power:7000},
+    {id: 1, name:"Yamcha", power:500},
+
   ]);
+
+  // powerClasses = computed(()=>
+  // { return{
+  //   'text-danger': true,
+  // }
+  // })
 
 }
