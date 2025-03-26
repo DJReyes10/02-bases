3759e870b002fe1b280da666757b747244a1a3c1
DJReyes10 <djreyesp@unah.hn>
2025-03-26 11:54:33 -0600

    Creación de componentes angular, como el inputs component y output

--- src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,5 +1,7 @@
 
 import { Component, signal } from '@angular/core';
+import { CharacterListComponent } from "../../components/dragonball/character-list/character-list.component";
+import { CharacterAppComponent } from "../../components/dragonball/character-add/character-add.component";
 
 interface Character {
   id: number;
@@ -11,6 +13,7 @@ interface Character {
   templateUrl: './dragonball-super-page.component.html',
   
   selector: 'dragonball-super',
+  imports: [CharacterListComponent, CharacterAppComponent],
   
 })
 export class DragonballSuperPageComponent {
@@ -22,26 +25,14 @@ export class DragonballSuperPageComponent {
     {id: 2, name:"Vegeta", power:8000},
 
   ]);
-
-  addCharacters(){
-    if( !this.name() || !this.power() || this.power() <= 0)
-    {
-      return
-    }
-    
-    const newCharacter: Character = {
-      id: this.characters().length +1,
-      name: this.name(),
-      power: this.power(),
-    };
-    this.characters.update((list) => 
-      [...list, newCharacter]);
-    this.resetFields();
-    }
-    resetFields(){
-      this.name.set('');
-      this.power.set(0);
-    }
+  addCharacters(character: Character){
+      this.characters.update((list) =>[...list, character])
+      }
+      resetFields(){
+        this.name.set('');
+        this.power.set(0);
+      }
   }
+
 
 
